6d3932f9759a5a0a4816f6fdad3f9b050132c4d6
santiago cortes <jeissoncortes02@outlook.com>
2021-11-08 09:00:39 -0500

    directivas

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,12 +1,20 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private _elementRef: ElementRef) {
+  @HostListener('mouseenter') onMouseEnter(){
     this._elementRef.nativeElement.style.backgroundColor = 'red'
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    this._elementRef.nativeElement.style.backgroundColor = ''
+  }
+
+
+  constructor(private _elementRef: ElementRef) {
+    //this._elementRef.nativeElement.style.backgroundColor = 'red'
    }
 
 }
